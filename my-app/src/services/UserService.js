590340c26b20f3494d6fa014fd73681c63f20c92
haseebsultan <38536290+haseebsultan@users.noreply.github.com>
2020-12-08 17:08:12 +0500

    Update UserService.js

--- my-app/src/services/UserService.js
+++ my-app/src/services/UserService.js
@@ -6,7 +6,7 @@ export async function getAllUsers() {
     //     .then(response => {return "haseeb";
     //         console.log(response);} );
 
-    const response = await fetch('http://10.0.210.133:3080/customers');
+    const response = await fetch('api/users');
     return await response.json();
     
 }
